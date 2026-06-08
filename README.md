47117f6d4fe85ff576959087f0e1069e7727f6c7
Gabriela de Andrade <166560478+gabrieladeandradecode@users.noreply.github.com>
2026-06-08 18:11:34 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# to-do-list-python-api
+Project A3 for University - Create API with Python using database, showing in Frontend with JavaScript and testing in Postman
---
+++ back-end/app.py
@@ -0,0 +1,52 @@
+from flask import Flask, jsonify, request
+# from flask_sqlalchemy import 
+
+
+app = Flask(__name__)
+
+tasks = [
+    {
+        'id': 1,
+        'title': 'Study Python',
+        'description': 'Create an API with Python'
+    },
+    {
+        'id': 2,
+        'title': 'Read API documentation',
+        'description': 'Learn parameters from API'
+    },
+    {
+        'id': 3,
+        'title': 'Study Javascript Asynchronous',
+        'description': 'Learn async, await and fetch'
+    }
+]
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify(tasks)
+
+@app.route('/tasks/<int:id>', methods=['GET'])
+def get_task_by_id(id):
+    for task in tasks:
+        if task.get('id') == id:
+            return jsonify(task)
+# CRUD = CREATE(POST), READ(GET), UPDATE(PUT) E DELETE 
+# MÉTODOS HTTP:
+# GET = PEGAR, LER
+# PUT = UPDATE, ATUALIZAR COMPLETO
+# POST = CRIAR, PUBLICAR
+# DELETE = EXCLUIR
+# PATCH = ATUALIZAR UMA PARTE 
+
+@app.route('/tasks/<int:id>', methods=['PUT'])
+def update_task_by_id(id):
+    updated_task = request.get_json()
+    for index, task in enumerate(tasks):
+        if task.get('id') == id:
+            tasks[index].update(updated_task)
+            return jsonify(tasks[index])
+
+
+
+app.run(port=5000, host='localhost', debug=True)
